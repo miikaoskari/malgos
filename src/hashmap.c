b40b75af416b61c2d0aa876b6bfd9adcf74de41b
miikaoskari <miika.karjalainen13@gmail.com>
2025-10-21 22:52:44 +0300

    Add replacement of value when key matches and handle chaining

--- src/hashmap.c
+++ src/hashmap.c
@@ -13,11 +13,11 @@ typedef struct mhash_entry_s
     mhash_entry_t *next;
 } mhash_entry_t;
 
-struct mhash_table_s
+typedef struct mhash_table_s
 {
     size_t bucket_count;
     mhash_entry_t **buckets;
-};
+} mhash_table_t;
 
 size_t mhash_init_key(char *key)
 {
@@ -26,7 +26,7 @@ size_t mhash_init_key(char *key)
     return sizeof(_key);
 }
 
-uint64_t mhash_calculate_hash(const char *value, const value_len)
+static inline uint64_t mhash_calculate_hash(const char *value, const value_len)
 {
     EVP_MD_CTX *ctx = EVP_MD_CTX_new();
     if (!ctx) return 1;
@@ -50,9 +50,9 @@ void mhash_create_table()
 
 }
 
-void mhash_get(struct mhash_table_s *hash_table, char *key)
+void mhash_get(struct mhash_table_s *hash_table, char *key, size_t key_len)
 {
-    uint64_t hash = mhash_calculate_hash(key);
+    uint64_t hash = mhash_calculate_hash(key, key_len);
     size_t idx = hash % hash_table->bucket_count;
 }
 
@@ -61,9 +61,9 @@ void mhash_delete(struct mhash_table_s *hash_table, char *key)
 
 }
 
-void mhash_put(struct mhash_table_s *hash_table, void *key, size_t key_len, void *value, size_t value_len)
+int mhash_put(struct mhash_table_s *hash_table, const void *key, const size_t key_len, const void *value, const size_t value_len)
 {
-    uint64_t hash = mhash_calculate_hash(value, value_len);
+    uint64_t hash = mhash_calculate_hash(key, key_len);
     size_t idx = hash % hash_table->bucket_count;
 
     mhash_entry_t *e = hash_table->buckets[idx];
@@ -71,29 +71,38 @@ void mhash_put(struct mhash_table_s *hash_table, void *key, size_t key_len, void
     {
         if (e->key_len == key_len && memcmp(e->key, key, key_len) == 0)
         {
-            /* collision */
+            /* same key! replace with new value */
+            char *new_value = realloc(e->value, value_len);
+            if (!new_value) return 1;
+            e->value = new_value;
+            memcpy(e->value, value, value_len);
+            e->value_len = value_len;
+            return 0;
         }
+
+        /* continue along the chain */
         e = e->next;
     }
 
     /* new entry */
     mhash_entry_t *ne = malloc(sizeof(*ne));
-    if (!ne) return 0;
+    if (!ne) return 1;
 
     /* allocate key */
     ne->key = malloc(key_len);
-    if (!ne->key) { free(ne); return 0; }
+    if (!ne->key) { free(ne); return 1; }
     memcpy(ne->key, key, key_len);
     ne->key_len = key_len;
 
     /* allocate value */
     ne->value = malloc(value_len);
-    if (!ne->value) { free(ne->key); free(ne); return 0; }
+    if (!ne->value) { free(ne->key); free(ne); return 1; }
     memcpy(ne->value, value, value_len);
     ne->value_len = value_len;
 
     /* set new entry to the bucket */
+    ne->next = hash_table->buckets[idx];
     hash_table->buckets[idx] = ne;
 
-    return;
+    return 0;
 }
