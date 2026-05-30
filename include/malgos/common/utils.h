cc831826cf4648beeaf62d01b9d1b47dadbe577a
miika <miika.karjalainen13@gmail.com>
2026-05-30 11:11:01 +0300

    add missing include

--- include/malgos/common/utils.h
+++ include/malgos/common/utils.h
@@ -1,7 +1,9 @@
 #ifndef MLG_UTILS_H
 #define MLG_UTILS_H
 
+#include <stddef.h>
+
 #define mlg_container_of(ptr, type, member) \
     ((type *)((char *)(ptr) - offsetof(type, member)))
 
-#endif // UTILS_H
+#endif // MLG_UTILS_H
